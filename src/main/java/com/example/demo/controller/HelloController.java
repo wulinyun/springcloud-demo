3f4409a570e30728e1c02be6e8c36a2dc1c355f7
wulinyun <lin.wu@landasoft.com>
2019-07-29 12:01:56 +0800

    更新调用链路监控配置

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,7 +1,9 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * @author 伍林云
@@ -9,9 +11,34 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class HelloController {
+    @Autowired
+    RestTemplate restTemplate;
 
     @GetMapping("/")
-    public  String test(){
-        return "88888888";
+    public String demoInfo()
+    {
+        return "demoInfo";
+    }
+    @GetMapping("/a/info")
+    public String a2c() {
+        return (String)this.restTemplate.getForObject("http://tracing-demo-c/", String.class, new Object[0]);
+    }
+    @GetMapping({"/c/info"})
+    public String cinfo() {
+        return (String)this.restTemplate.getForObject("http://tracing-demo-c/", String.class, new Object[0]);
+    }
+    @GetMapping({"/b/info"})
+    public String binfo() {
+        return (String)this.restTemplate.getForObject("http://tracing-demo-b/", String.class, new Object[0]);
+    }
+    @GetMapping({"/b2c"})
+    public String b2c() {
+        return (String)this.restTemplate.getForObject("http://tracing-demo-b/b2c", String.class, new Object[0]);
+    }
+    @GetMapping({"/layers"})
+    public String complex() {
+        String info1 = (String)this.restTemplate.getForObject("http://tracing-demo-b/b2c", String.class, new Object[0]);
+        String info2 = (String)this.restTemplate.getForObject("http://tracing-demo-c/", String.class, new Object[0]);
+        return info1 + " -> " + info2;
     }
 }
